fb95b9da18ab0834da145fdc5181c217385c2074
Claudio Lorina <claudio.lorina@gmail.com>
2020-12-08 18:44:24 +0100

    Lab7: fixed path generation
    - Added check in order to avoid paths from one node to the same node

--- main.py
+++ main.py
@@ -3,23 +3,34 @@ import matplotlib.pyplot as plt
 import random
 from include import virtualnet as vn
 
-net = vn.Network("./resources/node.json")
+net = vn.Network("./resources/nodes_full.json")
 net.connect()
 connection_data = pd.DataFrame(columns=["path", "snr", "latency"])
 connections = []
+
 nodes = list(net.nodes.keys())
 
 for i in range(100):
-    connections.append(vn.Connection(random.choice(nodes), random.choice(nodes), 1))
+    n1 = random.choice(nodes)
+    n2 = random.choice(nodes)
+    while n1 == n2:
+        n2 = random.choice(nodes)
+    connections.append(vn.Connection(n1, n2, 1))
 
 net.stream(connections[:50], False)
 net.stream(connections[50:], True)
-
+tot_snr = 0
+tot_lat = 0
 for conn in connections:
-    if conn.snr is not None:
+    if conn.latency is not None:
+        tot_snr += conn.snr
+        tot_lat += conn.latency
         connection_data = connection_data.append({"path": f"{conn.input}->{conn.output}",
                                                   "snr": conn.snr,
                                                   "latency": conn.latency}, ignore_index=True)
+print(f"Avg(snr): {tot_snr/len(connection_data)}")
+print(f"Avg(lat): {tot_lat/len(connection_data)}")
+print(f"N connections: {len(connection_data)}")
 plt.subplot(121)
 plt.xticks([])
 plt.plot(connection_data.path, connection_data.snr, "go")
